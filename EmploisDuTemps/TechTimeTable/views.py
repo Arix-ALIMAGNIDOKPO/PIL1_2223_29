c1c8e5de0310cc8b99a231daee02afdc97c52244
Arix ALIMAGNIDOKPO <arixalim@gmail.com>
2023-06-20 16:39:07 +0100

    MODIFS

--- EmploisDuTemps/TechTimeTable/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def Accueil(request):
-    return render(request,'Accueil.html')
-def Connexion(request):
-    return render(request,'Connexion.html')
-def Inscription(request):
-    return render(request,'Inscription.html')
