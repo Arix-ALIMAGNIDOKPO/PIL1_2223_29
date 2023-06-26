c4d21a25197865a7b188bb2d3cf97baeb52cbb51
Arix ALIMAGNIDOKPO <arixalim@gmail.com>
2023-06-26 11:27:09 +0100

    complete password reset

--- TechTime/urls.py
+++ TechTime/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path('se-deconnecter/', views.deconnexion, name='logout'),
     path('emplois/', views.Emplois, name='emplois'),
     #reinitialisation du mot de passe
-    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
-    path('reset_password_send', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name = 'Password_reset.html'), name='reset_password'),
+    path('reset_password_send', auth_views.PasswordResetDoneView.as_view(template_name = 'Password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = 'Password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name = 'Password_reset_done.html'), name='password_reset_complete')
 ]
